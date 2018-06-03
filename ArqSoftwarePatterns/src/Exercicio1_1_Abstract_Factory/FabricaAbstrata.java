d951e97179798c4f8efe80a011ccfef078c6da97
juliohds <tecjuliohenrique@gmail.com>
2018-06-03 00:47:43 -0300

    aula07

---
+++ ArqSoftwarePatterns/src/Exercicio1_1_Abstract_Factory/FabricaAbstrata.java
@@ -0,0 +1,12 @@
+package Exercicio1_1_Abstract_Factory;
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 1012578824676e5bc76d732b9c2403a6ec164e27
+
+public interface FabricaAbstrata {
+	
+	Impressao criarImpressao();
+
+}
