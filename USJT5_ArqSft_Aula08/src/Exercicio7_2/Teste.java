a94b1157e129723a603963d0df3dafb9cc89be61
juliohds <tecjuliohenrique@gmail.com>
2018-06-03 00:26:56 -0300

    Aula08

---
+++ USJT5_ArqSft_Aula08/src/Exercicio7_2/Teste.java
@@ -0,0 +1,11 @@
+package Exercicio7_2;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		double[] d = { 1.0, 1.1, 2.0, 2.1 };
+		ComparatorDoubles cd = new ComparatorDoubles();
+		cd.run(d);
+	}
+
+}
